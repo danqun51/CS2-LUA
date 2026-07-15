12b2fbb048d1175c85c4a9a7d5ff8ef042c73be0
danqun51 <62783714+danqun51@users.noreply.github.com>
2026-07-15 16:21:58 +0800

    Add files via upload

---
+++ build_vs2022.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+set "VS_ROOT=E:\Visual Studio\2022\Community"
+set "VCVARS=%VS_ROOT%\VC\Auxiliary\Build\vcvars64.bat"
+set "CMAKE=%VS_ROOT%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
+set "ROOT=%~dp0."
+
+if not exist "%VCVARS%" (
+  echo [ERROR] vcvars64.bat not found: %VCVARS%
+  exit /b 1
+)
+if not exist "%CMAKE%" (
+  echo [ERROR] cmake.exe not found: %CMAKE%
+  exit /b 1
+)
+
+call "%VCVARS%"
+pushd "%ROOT%"
+"%CMAKE%" -S . -B build -G "Visual Studio 17 2022" -A x64 || exit /b 1
+"%CMAKE%" --build build --config Release || exit /b 1
+popd
+endlocal
